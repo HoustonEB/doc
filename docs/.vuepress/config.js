a01afec7a1df554d53400ac39316b53490b5ae85
v_yuzhuang01 <v_yuzhuang01@baidu.com>
2018-10-12 15:22:19 +0800

    add text deal

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,70 +1,76 @@
 module.exports = {
-    "title": "VuePress",
-    "description": "Vue 驱动的静态网站生成器",
-    "base": "/doc/",
-    themeConfig: {
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'Guide', link: '/guide/' },
-            { text: 'External', link: 'https://baidu.com' },
-        ],
-        // sidebar: [
-        //     ['/guide/page-a', 'page-a.md'],
-        //     ['/guide/', 'README.md'],
-        // ],
-        sidebar: [
-            {
-              title: 'React',
-              // collapsable: false,
-              children: [
-                ['/React/State VS Props', 'State VS Props']
-              ]
-            },
-            {
-              title: 'JS',
-              children: [
-                ['/JS/面向对象编程', '面向对象编程'],
-                ['/JS/Module', 'Module']
-              ]
-            },
-            {
-              title: 'Mobx',
-              children: [
-                ['/Mobx/observable/objects', 'objects'],
-                ['/Mobx/observable/arrays', 'arrays']
+  "title": "VuePress",
+  "description": "Vue 驱动的静态网站生成器",
+  "base": "/docs/",
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://baidu.com' },
+    ],
+    // sidebar: [
+    //     ['/guide/page-a', 'page-a.md'],
+    //     ['/guide/', 'README.md'],
+    // ],
+    sidebar: [
+      {
+        title: 'Css',
+        children: [
+          ['/Css/文本处理', '文本处理']
+        ]
+      },
+      {
+        title: 'JS',
+        children: [
+          ['/JS/面向对象编程', '面向对象编程'],
+          ['/JS/Module', 'Module']
+        ]
+      },
+      {
+        title: 'React',
+        // collapsable: false,
+        children: [
+          ['/React/State VS Props', 'State VS Props']
+        ]
+      },
+      {
+        title: 'Mobx',
+        children: [
+          ['/Mobx/observable/objects', 'objects'],
+          ['/Mobx/observable/arrays', 'arrays']
 
-              ]
-            },
-            {
-              title: 'Redux',
-              children: [
-                ['/Redux/Reader', 'Reader']
-              ]
-            },
-            {
-              title: 'Echarts',
-              children: [
-                ['/Echarts/Echarts', 'Echarts']
-              ]
-            },
-            {
-              title: 'Git',
-              children: [
-                ['/Git/Branch', 'Branch']
-              ]
-            },
-            {
-              title: 'Webpack',
-              children: [
-                ['/Webpack/Webpack', 'Quick Start']
-              ]
-            },
-            {
-              title: 'Node',
-              children: [
-                ['/Node/Node', 'Node']
-              ]
-            }
-          ]
-    }
+        ]
+      },
+      {
+        title: 'Redux',
+        children: [
+          ['/Redux/Reader', 'Reader']
+        ]
+      },
+      {
+        title: 'Echarts',
+        children: [
+          ['/Echarts/Echarts', 'Echarts']
+        ]
+      },
+      {
+        title: 'Git',
+        children: [
+          ['/Git/Branch', 'Branch']
+        ]
+      },
+      {
+        title: 'Webpack',
+        children: [
+          ['/Webpack/Webpack', 'Quick Start']
+        ]
+      },
+      {
+        title: 'Node',
+        children: [
+          ['/Node/Node', 'Node']
+        ]
+      }
+    ]
+  }
 }
